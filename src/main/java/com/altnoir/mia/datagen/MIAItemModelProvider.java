a4424fe494f70695af19871e12c2e7723a54e067
RingLo_ <63995099+lonelyicer@users.noreply.github.com>
2025-01-28 16:14:45 +0800

    chore: clean import

--- src/main/java/com/altnoir/mia/datagen/MIAItemModelProvider.java
+++ src/main/java/com/altnoir/mia/datagen/MIAItemModelProvider.java
@@ -2,12 +2,7 @@ package com.altnoir.mia.datagen;
 
 import com.altnoir.mia.MIA;
 import com.altnoir.mia.register.MIAItems;
-import net.minecraft.core.Holder;
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.levelgen.GenerationStep;
-import net.neoforged.neoforge.client.model.generators.ItemModelBuilder;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
