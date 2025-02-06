e1a2e3d694613f2dc3a9e2235194083c28b211f6
Altnoir <2474515202@qq.com>
2025-02-06 09:32:19 +0800

    add chlorophyteore

---
+++ src/main/java/com/altnoir/mia/content/blocks/ChlorophyteOreBlock.java
@@ -0,0 +1,35 @@
+package com.altnoir.mia.content.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class ChlorophyteOreBlock extends Block {
+    public ChlorophyteOreBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public boolean isRandomlyTicking(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (random.nextInt(100) > 1) return;
+
+        for (Direction direction : Direction.values()) {
+            BlockPos offsetPos = pos.relative(direction);
+            BlockState offsetState = level.getBlockState(offsetPos);
+
+            if (offsetState.is(Blocks.MOSS_BLOCK)) {
+                level.setBlock(offsetPos, state, 3);
+                break;
+            }
+        }
+    }
+}
